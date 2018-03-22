6debd50d1de52c2f1107f4ba4b0c1070181bad4d
Manolis-Tasiopoulos <p15tasi@ionio.gr>
2018-03-22 12:42:00 +0200

    Update scanner.py

--- scanner.py
+++ scanner.py
@@ -1,79 +1,81 @@
+#------------------------------------------Dictionaries-----------------------------------------
+td = { 'q0':{'thrNine':'q1', 'zerOne':'q4', 'two':'q2' },
+       'q1':{ 'colon':'q3' },
+       'q2':{ 'colon':'q3', 'zerThree':'q1' },
+       'q3':{ 'zerFive':'q5',  },
+       'q4':{ 'colon':'q3', 'zerNine':'q2' },
+       'q5':{ 'zerNine':'q6' }             
+     } 
 
-def getchar(words,pos):
-	""" returns char at pos of words, or None if out of bounds """
 
-	if pos<0 or pos>=len(words): return None
+ad = { 'q6':'TIME_TOKEN' }
+#------------------------------------------Functions-------------------------------------------
+def getchar(times,pos):
+	
+	check_array = ['colon', 'thrNine', 'zerNine', 'zerThree','zerFive', 'zerOne', 'two']
+	check = False
 
-	return words[pos]
+	if pos < 0 or pos >= len(times): 		
+		check_array = []
+		return check_array
+	if not(ord(times[pos]) == ord(':') or ord(times[pos]) == ord('.')):
+		check_array.remove('colon')
+		check = True		
+	if not(ord(times[pos]) >= ord('3') and ord(times[pos]) <= ord('9')): 			
+		check_array.remove('thrNine')
+		check = True
+	if not(ord(times[pos]) >= ord('0') and ord(times[pos]) <= ord('9')):  	
+		check_array.remove('zerNine')
+		check = True
+	if not(ord(times[pos]) >= ord('0') and ord(times[pos]) <= ord('3')): 	
+		check_array.remove('zerThree')
+		check = True
+	if not(ord(times[pos]) >= ord('0') and ord(times[pos]) <= ord('5')): 	
+		check_array.remove('zerFive')
+		check = True
+	if not(ord(times[pos]) >= ord('0') and ord(times[pos]) <= ord('1')): 			
+		check_array.remove('zerOne')
+		check = True
+	if not(ord(times[pos]) == ord('2')): 			
+		check_array.remove('two')
+		check = True
+
+	if not check:
+		check_array = ['ERROR']
+	
+	return check_array
 	
 
 def scan(text,transition_table,accept_states):
-	""" Scans `text` while transitions exist in 'transition_table'.
-	After that, if in a state belonging to `accept_states`,
-	returns the corresponding token, else ERROR_TOKEN.
-	"""
 	
-	# initial state
 	pos = 0
 	state = 'q0'
 	
 	while True:
-		
-		c = getchar(text,pos)	# get next char
-		
-		if state in transition_table and c in transition_table[state]:
-		
-			state = transition_table[state][c]	# set new state
-			pos += 1	# advance to next char
-			
-		else:	# no transition found
-
-			# check if current state is accepting
+		check = False		
+		c = getchar(text,pos)				
+		for i in c:
+			if state in transition_table and i in transition_table[state] and not check:					
+					state = transition_table[state][i]	
+					pos += 1		
+					check = True
+					break											
+		if not check:
 			if state in accept_states:
 				return accept_states[state],pos
 
-			# current state is not accepting
-			return 'ERROR_TOKEN',pos
 			
-	
-# the transition table, as a dictionary
+			return 'ERROR_TOKEN',pos			
 
-# Αντικαταστήστε με το δικό σας λεξικό μεταβάσεων...
-td = { 'q0':{ 't':'q1','l':'q2' },
-       'q1':{ 'e':'q3' },
-       'q2':{ 'o':'q8' },
-       'q3':{ 's':'q4','r':'q6' },
-       'q4':{ 't':'q5' },
-       'q6':{ 'm':'q7' },
-       'q8':{ 'n':'q9' },
-       'q9':{ 'g':'q10'}
-     } 
+#------------------------------------------Main_Program------------------------------------------
+print('\n\n		***DFA TIME RECOGNITION***\n')				
+temp = input('		      -check: ')
 
-# the dictionary of accepting states and their
-# corresponding token
-
-# Αντικαταστήστε με το δικό σας λεξικό καταστάσεων αποδοχής...
-ad = { 'q5':'TEST_TOKEN',
-       'q7':'TERM_TOKEN',
-       'q10':'LONG_TOKEN'
-     }
-
-
-# get a string from input
-text = input('give some input>')
-
-# scan text until no more input
-while text:	# that is, while len(text)>0
+while temp:		
+	token,position = scan(temp,td,ad)
+	print('\n		     ---RESAULTS---')
 	
-	# get next token and position after last char recognized
-	token,position = scan(text,td,ad)
-	
-	if token=='ERROR_TOKEN':
-		print('unrecognized input at pos',position+1,'of',text)
-		break
-	
-	print("token:",token,"string:",text[:position])
-	
-	# remaining text for next scan
-	text = text[position:]
-	
+	print("	             string:",temp)
+	print("	             token: ",token)
+	print('	             --------------\n')		
+	break
